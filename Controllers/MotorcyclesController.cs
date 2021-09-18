6f734e06f76a0c99e602a601a42582d0c953fbd5
Dima Vladiko <dimavladi31@gmail.com>
2021-09-18 22:15:50 +0300

    Dima - Started Controllers - 18.9.21

--- Controllers/MotorcyclesController.cs
+++ Controllers/MotorcyclesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -44,6 +45,7 @@ namespace motoShop.Controllers
         }
 
         // GET: Motorcycles/Create
+        [Authorize(Roles = "Admin,Employee")]
         public IActionResult Create()
         {
             return View();
@@ -66,6 +68,7 @@ namespace motoShop.Controllers
         }
 
         // GET: Motorcycles/Edit/5
+        [Authorize(Roles = "Admin,Employee")]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -117,6 +120,7 @@ namespace motoShop.Controllers
         }
 
         // GET: Motorcycles/Delete/5
+        [Authorize(Roles = "Admin,Employee")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
