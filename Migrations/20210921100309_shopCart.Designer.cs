e7d920537ed0290950b37174957dc4e7c6658af5
gosha-shapira <52867890+gosha-shapira@users.noreply.github.com>
2021-09-21 15:21:39 +0300

    ShoppingCart with messing aroud. Database is not yet set

--- Migrations/20210921100309_shopCart.Designer.cs
+++ Migrations/20210921100309_shopCart.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using motoShop.Data;
 
 namespace motoShop.Migrations
 {
     [DbContext(typeof(motoShopContext))]
-    partial class motoShopContextModelSnapshot : ModelSnapshot
+    [Migration("20210921100309_shopCart")]
+    partial class shopCart
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
